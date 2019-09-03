097e8197a64e7dd047f087baee234382e200e7ed
Aitor <aitor@bodas.net>
2019-09-03 17:28:10 +0200

    Restart jenkins before groovy - test 1

--- spec/defines/jenkins_cli_exec_spec.rb
+++ spec/defines/jenkins_cli_exec_spec.rb
@@ -18,6 +18,96 @@ describe 'jenkins::cli::exec' do
           is_expected.to contain_jenkins__cli__exec('foo').
             that_comes_before('Anchor[jenkins::end]')
         end
+
+  let(:facts) do
+    {
+      osfamily: 'RedHat',
+      operatingsystem: 'RedHat', # require by puppetlabs/java
+      operatingsystemrelease: '6.7',
+      operatingsystemmajrelease: '6'
+    }
+  end
+
+  let(:helper_cmd) { 'systemctl restart jenkins && /bin/cat /usr/lib/jenkins/puppet_helper.groovy | /usr/bin/java -jar /usr/lib/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080 groovy =' }
+
+  describe 'relationships' do
+    it do
+      is_expected.to contain_jenkins__cli__exec('foo').
+        that_requires('Class[jenkins::cli_helper]')
+    end
+    it do
+      is_expected.to contain_jenkins__cli__exec('foo').
+        that_comes_before('Anchor[jenkins::end]')
+    end
+  end
+
+  describe 'title =>' do
+    context 'foo' do
+      # default title...
+
+      it do
+        is_expected.to contain_exec('foo').with(
+          command: "#{helper_cmd} foo",
+          tries: 10,
+          try_sleep: 10,
+          unless: nil
+        )
+      end
+      it { is_expected.to contain_exec('foo').that_notifies('Class[jenkins::cli::reload]') }
+    end
+
+    context 'bar' do
+      let(:title) { 'bar' }
+
+      it do
+        is_expected.to contain_exec('bar').with(
+          command: "#{helper_cmd} bar",
+          tries: 10,
+          try_sleep: 10,
+          unless: nil
+        )
+      end
+      it { is_expected.to contain_exec('bar').that_notifies('Class[jenkins::cli::reload]') }
+    end
+  end # title =>
+
+  describe 'command =>' do
+    context 'bar' do
+      let(:params) { { command: 'bar' } }
+
+      it do
+        is_expected.to contain_exec('foo').with(
+          command: "#{helper_cmd} bar",
+          tries: 10,
+          try_sleep: 10,
+          unless: nil
+        )
+      end
+    end
+
+    context "['bar']" do
+      let(:params) { { command: %w[bar] } }
+
+      it do
+        is_expected.to contain_exec('foo').with(
+          command: "#{helper_cmd} bar",
+          tries: 10,
+          try_sleep: 10,
+          unless: nil
+        )
+      end
+    end
+
+    context "['bar', 'baz']" do
+      let(:params) { { command: %w[bar baz] } }
+
+      it do
+        is_expected.to contain_exec('foo').with(
+          command: "#{helper_cmd} bar baz",
+          tries: 10,
+          try_sleep: 10,
+          unless: nil
+        )
       end
 
       describe 'title =>' do
